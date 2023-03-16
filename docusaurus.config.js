228f1af553d5fbd0d91e4e0f4bb27fdf6777d820
JN <48717503+JN-00@users.noreply.github.com>
2023-03-16 12:55:11 +0000

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://jn-00.github.io/Profile',
-  baseUrl: '/',
+  url: 'https://jn-00.github.io/',
+  baseUrl: '/Profile',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
